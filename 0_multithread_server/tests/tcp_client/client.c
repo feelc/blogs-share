aaae9850e0a38e02181609c2ad93a717f76dd7ec
wanghao <wanghao.1024@bytedance.com>
2021-04-09 17:09:49 +0800

    fix:memset

--- 0_multithread_server/tests/tcp_client/client.c
+++ 0_multithread_server/tests/tcp_client/client.c
@@ -20,7 +20,7 @@ int32_t main(int argc, char const *argv[])
         goto ERR;
     }
 
-    memset(&server_addr, sizeof(server_addr), 0);
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_addr.sin_port = htons(55555);

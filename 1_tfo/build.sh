bb38415455c6a997d117b7cbe28f3a0b2ea3e873
wanghao <wanghao.1024@bytedance.com>
2021-04-15 21:43:30 +0800

    update

--- 1_tfo/build.sh
+++ 1_tfo/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ -o  client client.cpp -DFAST_OPEN
-g++ -o  client server.cpp -DFAST_OPEN
+g++ -o client client.cpp -DFAST_OPEN
+g++ -o server server.cpp -DFAST_OPEN
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # blogs-share
 博客中用到的代码
 
-0_multithread_server 测试SO_REUSEPORT的负载均衡功能
+0_multithread_server 测试SO_REUSEPORT的负载均衡功能     
 1_tfo 测试tcp fast open的功能
